6e9e422bd56fa9932552259fb808114b2fff94b8
Chen Li <li1.chen@intel.com>
2015-09-23 15:34:44 +0800

    add passrate for jerry 6

--- driver/passrate.py
+++ driver/passrate.py
@@ -49,7 +49,7 @@ cset = "%s" % datetime.date.today()
 mode = Mode(shell, args, os.environ.copy(), name, cset)
 
 # Change test benchmark to passrate
-benchmarks.Benchmarks = [builders.JerryPassrate()]
+benchmarks.Benchmarks = [benchmarks.JerryPassrate()]
 
 # Set of slaves that run the builds. 
 KnownSlaves = slaves.init()
