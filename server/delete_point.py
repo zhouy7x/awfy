1c95c110f3d166e4fbf5b629313e22b778589f96
haijie <haijiex.li@intel.com>
2018-12-04 09:55:04 +0000

    add a script of delete database point.

---
+++ server/delete_point.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import replace_all
+
+point_ls = [157718, 157719, 157720]
+machine_id = 6
+replace_all.delete_all(point_ls, machine_id)
+
+# point_ls = [ 152972, 153105, 153098, 152161]
+# machine_id = 10
+# replace_all.delete_all(point_ls, machine_id)
