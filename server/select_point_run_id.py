cf64cee4b0ec4d0272228a7e04aa0ade2505ad04
haijie <haijiex.li@intel.com>
2019-08-12 13:10:29 +0800

    feat:   add stop stamp in server/select_point_run_id.py

--- server/select_point_run_id.py
+++ server/select_point_run_id.py
@@ -17,34 +17,41 @@ Manual to the script of %s, you need:
     
    - A mode id:
    
-        -mode=34
+        --mode=34
         
-   - A number of stamp:
+   - A number of start stamp:
      
-        -stamp=1555082061
+        --startstamp=1555082061
+        
+   - A number of stop stamp:
      
-   - A command in terminal, you can use simple name for each config("-m" for "--machine", "-o" for "--mode", "-s" for 
-     "--stamp"):
+        --stopstamp=1555082062
+        
+     (default is: none, means till the end.)
+   - A command in terminal, you can use simple name for each config("-m" for "--machine", 
+     "-o" for "--mode", "-st" for "--startstamp", "-sp" for "--stopstamp"):
 
-        python select_point_run_id.py -m 1 -o 10 -s 1555082061
+        python select_point_run_id.py -m 1 -o 10 -st 1555082061 -sp 1555082062
 
 
 Examples:
 
-      python select_point_run_id.py -m 1 -o 10 -s 1555082061
-      python select_point_run_id.py -machine=1 -mode=10 -stamp=1555082061
+      python select_point_run_id.py -m 1 -o 10 -st 1555082061
+      python select_point_run_id.py --machine=1 --mode=10 --startstamp=1555082061 --stopstamp=1555082062
 
 """ % __file__
 parser = argparse.ArgumentParser(description='------')
 parser.usage = help
 parser.add_argument('-m', '--machine', type=int, required=True, help="machine id")
 parser.add_argument('-o', '--mode', type=int, required=True, help="mode id")
-parser.add_argument('-s', '--stamp', type=int, required=True, help="stamp number")
+parser.add_argument('-st', '--startstamp', type=int, required=True, help="start stamp number")
+parser.add_argument('-sp', '--stopstamp', type=int, default=None, help="stop stamp number")
 
 args = parser.parse_args()
 machine = args.machine
 mode = args.mode
-stamp = args.stamp
+start_stamp = args.startstamp
+stop_stamp = args.stopstamp
 
 
 def write_to_file(data, a1, a2):
@@ -52,18 +59,31 @@ def write_to_file(data, a1, a2):
         f.write(json.dumps(data))
 
 
-def main(machine_id, mode_id, start_stamp):
-    query = """
-    SELECT STRAIGHT_JOIN *          
-          FROM awfy_run r    
-                JOIN awfy_build b ON r.id = b.run_id       
-          WHERE  r.machine = %s 
-                and b.mode_id=%s
-                and r.stamp >= %s  
-          ORDER BY r.stamp DESC;
-    """
+def main(machine_id, mode_id, startstamp, stopstamp=None):
     c = awfy.db.cursor()
-    lines = c.execute(query, [machine_id, mode_id, start_stamp])
+    if stopstamp:
+        query = """
+        SELECT STRAIGHT_JOIN *          
+              FROM awfy_run r    
+                    JOIN awfy_build b ON r.id = b.run_id       
+              WHERE  r.machine = %s 
+                    and b.mode_id=%s
+                    and r.stamp >= %s  
+                    and r.stamp <= %s  
+              ORDER BY r.stamp DESC;
+        """
+        lines = c.execute(query, [machine_id, mode_id, startstamp, stopstamp])
+    else:
+        query = """
+        SELECT STRAIGHT_JOIN *          
+              FROM awfy_run r    
+                    JOIN awfy_build b ON r.id = b.run_id       
+              WHERE  r.machine = %s 
+                    and b.mode_id=%s
+                    and r.stamp >= %s  
+              ORDER BY r.stamp DESC;
+        """
+        lines = c.execute(query, [machine_id, mode_id, startstamp])
     print (lines)
     a = c.fetchall()
     # print a
@@ -77,4 +97,4 @@ def main(machine_id, mode_id, start_stamp):
 
 
 if __name__ == '__main__':
-    main(machine, mode, stamp)
+    main(machine, mode, start_stamp, stop_stamp)
