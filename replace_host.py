14412530a911117ece3f85903ee1ef31530966d7
haijie <haijiex.li@intel.com>
2019-10-25 10:06:05 +0800

    test:   optmize replace_host.py

--- replace_host.py
+++ replace_host.py
@@ -12,16 +12,17 @@ try:
 except Exception as e:
     print "Error: Must give 2 params, old host and new host."
 else:
-    cmd = "grep -nr 'ssgs5-test.sh.intel.com' > host-log.txt"
-    os.system(cmd)
-    with open('host-log.txt') as f:
-        data = f.readlines()
+    cmd = "grep -nr '%s' ." % old_host
+    data = os.popen(cmd).readlines()
+    # with open('host-log.txt') as f:
+    #     data = f.readlines()
     path_list = map(lambda x: x[:x.find(':')], data)
+    print data
     # replace old host to new host.
-    for path in path_list:
-        cmd = 'perl -pi -e "s/%s/%s/g" %s' % (old_host, new_host, path)
-        print cmd
-        os.system(cmd)
+    # for path in path_list:
+    #     cmd = 'perl -pi -e "s/%s/%s/g" %s' % (old_host, new_host, path)
+    #     print cmd
+    #     os.system(cmd)
 
     # remove *.pyc
     # cmd = 'find / -name "*.pyc" -print | xargs rm'
