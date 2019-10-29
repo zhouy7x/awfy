45b8a6e8ff5ffdf17f23956a27053833f1a15063
haijie <haijiex.li@intel.com>
2019-10-29 13:53:33 +0800

    fix:   fix bug no such file or directory ./website/data/

--- server/run-clean-and-update.sh
+++ server/run-clean-and-update.sh
@@ -7,7 +7,7 @@ then
 fi
 
 touch /tmp/awfy-lock
-rm -rf /home/user/work/awfy/website/data/* 
-#cp /home/user/work/awfy/website/master.js /home/user/work/awfy/website/data
+rm -rf /home/user/work/awfy/website/data/
+mkdir -p /home/user/work/awfy/website/data/
 /usr/bin/python /home/user/work/awfy/server/update.py
 rm /tmp/awfy-lock
