2f0aae2139be1d1db9249281abb5406546ca5e1b
haijie <haijiex.li@intel.com>
2019-12-23 14:15:42 +0800

    feat:   run the same commit for long time bench

--- driver/schedule-run-1800x.sh
+++ driver/schedule-run-1800x.sh
@@ -15,8 +15,31 @@
 #     screen -r
 #     (wait for it to confirm that it's no longer running)
 #     ctrl a+d
+
+function list_include_item(){
+  local list=`ls $1`
+  local item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item, reset the freq.
+    echo $v8_longtime_bench_freq > $v8countfile
+  fi
+}
+
+function create_position(){
+    if [ ! -e $v8_longtime_bench_commit_dir ]; then
+        mkdir -p $v8_longtime_bench_commit_dir
+    fi
+    pushd $v8_longtime_bench_commit_dir
+    touch $1
+    popd
+}
+
+v8_longtime_bench_commit_dir=tmp/v8_1800x_longtime_bench_commit
+base_v8_longtime_bench_commit_dir=tmp/v8_longtime_bench_commit
 lockfile=/tmp/awfy-daemon-1800x
 v8countfile=tmp/1800x-v8-count
+v8_longtime_bench_freq=70
+
 if [ -e "$lockfile" ]
 then
   echo "awfy: Already running"
@@ -30,7 +53,6 @@ trap "kill 0" EXIT
 python print_env.py
 
 
-count=0
 while :
 do
     if [ -e /tmp/awfy ]
@@ -59,6 +81,10 @@ do
 
                 STARTT=$(date +%s)
 
+                echo $id
+                #1800x runs slower than v8, so let 1800x change the longtime bench freq.
+                list_include_item $base_v8_longtime_bench_commit_dir $id
+
                 if [ ! -e $v8countfile ]; then
                     touch $v8countfile
                 fi
@@ -68,7 +94,8 @@ do
                 fi
                 echo $tmp;
 
-                if [ $tmp == 70 ]; then
+                if [ $tmp == $v8_longtime_bench_freq ]; then
+                    create_position $id;
                     string='-long-time';
                     tmp=0
                 else
--- driver/schedule-run-v8.sh
+++ driver/schedule-run-v8.sh
@@ -15,9 +15,33 @@
 #     screen -r
 #     (wait for it to confirm that it's no longer running)
 #     ctrl a+d
+
+function list_include_item(){
+  local list=`ls $1`
+  local item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item, reset the freq.
+    echo $v8_longtime_bench_freq > $v8countfile
+  fi
+}
+
+function create_position(){
+    if [ ! -e $v8_longtime_bench_commit_dir ]; then
+        mkdir -p $v8_longtime_bench_commit_dir
+    fi
+    pushd $v8_longtime_bench_commit_dir
+    touch $1
+    popd
+}
+
+v8_longtime_bench_commit_dir=tmp/v8_longtime_bench_commit
+base_v8_longtime_bench_commit_dir=tmp/v8_1800x_longtime_bench_commit
 lockfile=/tmp/awfy-daemon-v8
 v8countfile=tmp/v8-count
 jsccountfile=tmp/jsc-count
+v8_longtime_bench_freq=70
+jsc_longtime_bench_freq=70
+
 if [ -e "$lockfile" ]
 then
   echo "awfy: Already running"
@@ -59,6 +83,10 @@ do
 
                 STARTT=$(date +%s)
 
+                echo $id
+                #1800x runs slower than v8, so let 1800x change the longtime bench freq.
+                #list_include_item $base_v8_longtime_bench_commit_dir $id
+
                 if [ ! -e $v8countfile ]; then
                     touch $v8countfile
                 fi
@@ -68,7 +96,8 @@ do
                 fi
                 echo $tmp;
 
-                if [ $tmp == 70 ]; then
+                if [ $tmp == $v8_longtime_bench_freq ]; then
+                    create_position $id;
                     string='-long-time';
                     tmp=0
                 else
@@ -179,7 +208,7 @@ do
                 fi
                 echo $tmp;
 
-                if [ $tmp == 70 ]; then
+                if [ $tmp == $jsc_longtime_bench_freq ]; then
                     string='-long-time';
                     tmp=0
                 else
